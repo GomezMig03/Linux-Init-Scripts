de2214aaaf057d488f65e1b6fda51583e4a8a483
gomezmig03 <gomezmig03@gmail.com>
2024-06-11 22:31:18 +0200

    Change flatpak floorp for tarball version

--- FedoraGamingDev.sh
+++ FedoraGamingDev.sh
@@ -82,8 +82,33 @@ dnf install vlc -y
 flatpak install -y flathub com.obsproject.Studio
 flatpak install -y flathub com.sindresorhus.Caprine
 flatpak install -y flathub com.parsecgaming.parsec
-flatpak install -y flathub one.ablaze.floorp
 flatpak install -y flathub net.davidotek.pupgui2 # ProtonUp-Qt
+
+# Download last version of Floorp
+floorp_version=$(curl -sL https://api.github.com/repos/Floorp-Projects/Floorp/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | sed 's/^v//')
+wget "https://github.com/Floorp-Projects/Floorp/releases/download/v${floorp_version}/floorp-${floorp_version}.linux-x86_64.tar.bz2"
+tar -xjvf "floorp-${floorp_version}.linux-x86_64.tar.bz2"
+rm -r "floorp-${floorp_version}.linux-x86_64.tar.bz2"
+mv "floorp" /usr/local/bin/
+rm -r "floorp"
+
+# Create the Floorp desktop file
+floorp_file="/usr/share/applications/floorp.desktop"
+cat > $floorp_file << EOF
+[Desktop Entry]
+Type=Application
+Name=Floorp
+Exec=/usr/local/bin/floorp/floorp %f
+Icon=/usr/local/bin/floorp/browser/chrome/icons/default/default128.png
+Comment= A Browser built for keeping the Open, Private and Sustainable Web alive. Based on Mozilla Firefox. 
+Categories=Network;WebBrowser;
+Terminal=false
+StartupNotify=true
+StartupWMClass=floorp
+EOF
+
+chmod +x $floorp_file
+update-desktop-database
 
 # Install Heroic Games Launcher
 latest_version=$(curl -sL https://api.github.com/repos/Heroic-Games-Launcher/HeroicGamesLauncher/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | sed 's/^v//')
