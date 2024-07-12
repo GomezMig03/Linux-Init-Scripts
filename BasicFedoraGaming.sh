12d2cb11ee8c8dac8b3c53986232256120996d83
gomezmig03 <gomezmig03@gmail.com>
2024-07-12 16:54:40 +0200

    Add BasicFedoraGaming script

---
+++ BasicFedoraGaming.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# Ensure the script is run with superuser privileges
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# Adds faster updates
+dnf_directory="/etc/dnf/dnf.conf"
+cat > "$dnf_directory" << EOF
+[main] 
+gpgcheck=1 
+installonly_limit=3 
+clean_requirements_on_remove=True 
+best=False 
+skip_if_unavailable=True 
+fastestmirror=1 
+max_parallel_downloads=10 
+deltarpm=true
+EOF
+
+# Get Fedora version
+fedora_version=$(rpm -E %fedora)
+
+# Add RPM Fusion repo
+dnf install -y "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${fedora_version}.noarch.rpm" \
+                "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${fedora_version}.noarch.rpm"
+dnf update -y
+dnf upgrade -y --refresh
+
+# Check if user has nvidia gpu
+gpu=$(lspci | grep -E -i "nvidia" | grep -E "VGA|3D");
+
+# Installation of nvidia drivers
+if [ -n "$gpu" ]; then
+while true; do
+        read -rp "NVIDIA card found, do you wish to install NVIDIA proprietary drivers? (Y/n): " res
+        case $res in
+            [Yy]* | "" )
+                echo "Installing drivers..."
+                dnf install akmod-nvidia -y
+                dnf install xorg-x11-drv-nvidia-cuda -y
+                dnf install xorg-x11-drv-nvidia-cuda-libs -y
+                break;
+                ;;
+            [Nn]* )
+                echo "NVIDIA drivers won't be installed"
+                break;
+                ;;
+            *)
+                echo "Please, enter a valid value (Y/n)"
+                ;; 
+        esac
+    done
+fi
+
+# Add Flathub repo
+dnf install -y flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# Enable openh264
+dnf config-manager --enable fedora-cisco-openh264
+
+# Add Wine's repository
+dnf config-manager --add-repo "https://dl.winehq.org/wine-builds/fedora/${fedora_version}/winehq.repo"
+
+# Install v4l2loopback kernel module for virtual camera support on OBS
+dnf install -y kmod-v4l2loopback
+
+# Install various packages
+dnf install steam -y
+dnf install lutris -y
+dnf install discord -y
+dnf install git -y
+dnf install wine -y
+
+flatpak install -y flathub net.davidotek.pupgui2 # ProtonUp-Qt
+
+# Install Heroic Games Launcher
+
+read -p "Do you want to install Heroic Games launcher? (y/N):" userSelect1
+
+if [ $userSelect1 = "y" ]; then 
+    latest_version=$(curl -sL https://api.github.com/repos/Heroic-Games-Launcher/HeroicGamesLauncher/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | sed 's/^v//')
+    wget "https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v${latest_version}/heroic-${latest_version}.x86_64.rpm"
+    dnf install -y "heroic-${latest_version}.x86_64.rpm"
+    rm "heroic-${latest_version}.x86_64.rpm"
+fi
+
+read -p "Do you want to install EmuDeck prerequisites? (y/N):" userSelect2
+
+if [ $userSelect2 = "y" ]; then 
+    dnf install -y jq rsync unzip zenity
+fi
