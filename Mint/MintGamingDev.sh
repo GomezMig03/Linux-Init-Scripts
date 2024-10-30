a0bdca89d6e6aa7cbee926ca4788ec984602f8a5
gomezmig03 <gomezmig03@gmail.com>
2024-10-30 07:56:16 +0100

    Add steam and lutris to Mint

--- Mint/MintGamingDev.sh
+++ Mint/MintGamingDev.sh
@@ -12,8 +12,18 @@ fi
 
 flatpak remote-add --if-not-exists --user launcher.moe https://gol.launcher.moe/gol.launcher.moe.flatpakrepo
 
+steam_deb="$(mktemp --suffix=.deb)"
+lutris_deb="$(mktemp --suffix=.deb)"
+
 apt update -y
+apt install git -y
 apt install wine -y
-wget -qO- "https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb" | sudo dpkg -i -
+
+wget -q "https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb" -O "$steam_deb"
+apt install -y "$steam_deb"
+rm -r "$steam_deb"
+
 last_lutris=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | grep -oP '"tag_name": "\K(.*)(?=")' | sed 's/^v//')
-wget -qO- "https://github.com/lutris/lutris/releases/download/v${last_lutris}/lutris_${last_lutris}_all.deb"
+wget -q "https://github.com/lutris/lutris/releases/download/v${last_lutris}/lutris_${last_lutris}_all.deb" -O "$lutris_deb"
+apt install -y "$lutris_deb"
+rm -r "$lutris_deb"
