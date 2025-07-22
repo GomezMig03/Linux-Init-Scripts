01c0207a12d2a99d3bc3493e54833090d72c838b
Miguel Gómez <gomezmig03@gmail.com>
2025-07-22 09:33:46 +0200

    Add convert subs from nenya

---
+++ Utilities/convert-nenyasubs.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -euo pipefail
+shopt -s nullglob
+
+dir="$(dirname "$1")"
+
+if [ -d "$dir/temp1" ]; then
+	rm -rf "$dir/temp1"
+	mkdir -p "$dir/temp1"
+fi
+
+unzip -q "$1" -d ./temp1/
+#rm "$1"
+
+item=$(ls ./temp1/ | head -n1)
+cd "$dir"/temp1/
+
+shopt -s extglob
+
+case "$item" in
+	*.zip)
+		unzip -q "$item" -d "./"
+	;;
+	*.rar)
+		unrar x -inul "$item" "./"
+	;;
+	*.7z)
+		7z x -y "$item"
+	;;
+	*)
+		echo "no se encontró comprimido dentro del comprimido principal:"
+		echo "$item"
+	;;
+esac
+rm "$item"
+
+names=( ./*esES.ass )
+name="${names[0]:-}"
+if [ -z "$name" ]; then
+	names=( ./*Español\ \(España\).ass* )
+	name="${names[0]:-}"
+fi
+if [ -z "$name" ]; then
+	names=( ./*spa.ass* )
+	name="${names[0]:-}"
+fi
+if [ -z "$name" ]; then
+	echo "Error encontrando archivo .ass"
+fi
+
+file="${name#./}"
+nombre="${name%.*}"
+
+ffmpeg -i "${file}" -c:s srt "$dir/../$nombre.srt"
