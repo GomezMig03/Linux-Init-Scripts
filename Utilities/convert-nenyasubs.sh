903cefd62d5a66f5fae4fb817ee9bfacad2dfaf5
Miguel <gomezmig03@gmail.com>
2025-08-02 15:11:53 +0200

    Add option to subs script to accept more than one file

--- Utilities/convert-nenyasubs.sh
+++ Utilities/convert-nenyasubs.sh
@@ -4,74 +4,81 @@ shopt -s nullglob
 
 dir="$(pwd)"
 
-if [ -d "$dir/temp1" ]; then
-	rm -rf "$dir/temp1"
-	mkdir -p "$dir/temp1"
-fi
+convert_subs() {
+	if [ -d "$dir/temp1" ]; then
+		rm -rf "$dir/temp1"
+		mkdir -p "$dir/temp1"
+	fi
 
-unzip -q "$1" -d ./temp1/
-rm "$1"
+	unzip -q "$1" -d ./temp1/
+	rm "$1"
 
-item=$(ls ./temp1/ | head -n1)
-cd "$dir"/temp1/
+	item=$(ls ./temp1/ | head -n1)
+	cd "$dir"/temp1/
 
-shopt -s extglob
+	shopt -s extglob
 
-case "$item" in
-	*.zip)
-		unzip -q "$item" -d "./"
-		rm "$item"
-	;;
-	*.rar)
-		unrar x -inul "$item" "./"
-		rm "$item"
-	;;
-	*.7z)
-		7z x -y "$item"
-		rm "$item"
-	;;
-	*.ass)
-	;;
-	*)
-		echo "no se encontró comprimido dentro del comprimido principal:"
-		echo "$item"
-	;;
-esac
+	case "$item" in
+		*.zip)
+			unzip -q "$item" -d "./"
+			rm "$item"
+		;;
+		*.rar)
+			unrar x -inul "$item" "./"
+			rm "$item"
+		;;
+		*.7z)
+			7z x -y "$item"
+			rm "$item"
+		;;
+		*.ass)
+		;;
+		*)
+			echo "no se encontró comprimido dentro del comprimido principal:"
+			echo "$item"
+		;;
+	esac
 
-names=( ./*esES.ass )
-name="${names[0]:-}"
-if [ -z "$name" ]; then
-	names=( ./*Español\ \(España\).ass* )
+	names=( ./*esES.ass )
 	name="${names[0]:-}"
-fi
-if [ -z "$name" ]; then
-	names=( ./*spa.ass* )
-	name="${names[0]:-}"
-fi
-if [ -z "$name" ]; then
-	names=( ./*es.ass* )
-	name="${names[0]:-}"
-fi
-if [ -z "$name" ]; then
-	names=( ./*.ass* )
-	name="${names[0]:-}"
-fi
+	if [ -z "$name" ]; then
+		names=( ./*Español\ \(España\).ass* )
+		name="${names[0]:-}"
+	fi
+	if [ -z "$name" ]; then
+		names=( ./*spa.ass* )
+		name="${names[0]:-}"
+	fi
+	if [ -z "$name" ]; then
+		names=( ./*es.ass* )
+		name="${names[0]:-}"
+	fi
+	if [ -z "$name" ]; then
+		names=( ./*.ass* )
+		name="${names[0]:-}"
+	fi
 
-if [ -z "$name" ]; then
-	echo "Error encontrando archivo .ass"
-fi
+	if [ -z "$name" ]; then
+		echo "Error encontrando archivo .ass"
+	fi
 
-file="${name#./}"
-nombre="${name%.*}"
+	file="${name#./}"
+	nombre="${name%.*}"
 
-ffmpeg -i "${file}" -c:s srt "$dir/$nombre-temp.srt"
+	ffmpeg -i "${file}" -c:s srt "$dir/$nombre-temp.srt"
+
+	awk '
+	BEGIN { RS=""; ORS="\n\n" }
+	/{\\an8}/ { next }
+	{ print }
+	' "$dir/$nombre-temp.srt" > "$dir/$nombre.srt"
+
+	rm "$dir/$nombre-temp.srt"
+	cd ..
+}
+
+for sub in "$@"; do
+	convert_subs "$sub"
+done
 
 rm -rf "$dir/temp1"
-
-awk '
-BEGIN { RS=""; ORS="\n\n" }
-/{\\an8}/ { next }
-{ print }
-' "$dir/$nombre-temp.srt" > "$dir/$nombre.srt"
-
-rm "$dir/$nombre-temp.srt"
